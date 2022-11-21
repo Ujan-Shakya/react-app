ef11806775ab2bdc682d88980a3e432a04c65393
ujan.shakya <ujan.shakya@gurzu.com>
2022-11-21 11:47:20 +0545

    updated test.yml

---
+++ .infra-script/push.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && sudo aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+sudo aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin $ECR_LOGIN_PASSWORD
+sudo docker tag $GITHUB_REPOSITORY:latest $ECR_REPOSITORY_URL:$GITHUB_REF_NAME
+sudo docker push $ECR_REPOSITORY_URL:$env.GITHUB_REF_NAME
+sudo docker image prune -f
