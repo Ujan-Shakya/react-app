f1368382b75ab924e5261818d64cb06b21b57484
ujan.shakya <ujan.shakya@gurzu.com>
2022-11-21 12:56:39 +0545

    updated test.yml

--- .infra-script/push.sh
+++ .infra-script/push.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+echo"AWS Configure"
 sudo aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && sudo aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+echo"Docker login"
 sudo aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin $ECR_LOGIN_PASSWORD
+echo"Docker tag"
 sudo docker tag $GITHUB_REPOSITORY:latest $ECR_REPOSITORY_URL:$GITHUB_REF_NAME
-sudo docker push $ECR_REPOSITORY_URL:$env.GITHUB_REF_NAME
-sudo docker logout
+echo"Docker push"
+sudo docker push $ECR_REPOSITORY_URL:$GITHUB_REF_NAME
+echo"Docker prune"
 sudo docker image prune -f
