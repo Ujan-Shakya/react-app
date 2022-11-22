e0c6b1871355ac70cc7024d57831af3d59cbbabe
ujan.shakya <ujan.shakya@gurzu.com>
2022-11-22 12:14:24 +0545

    updated test.yml

--- .infra-script/push.sh
+++ .infra-script/push.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -o pipefail
+
 sudo aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID && sudo aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 
 sudo aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin $ECR_LOGIN_PASSWORD
